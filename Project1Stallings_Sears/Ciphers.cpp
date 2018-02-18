4e21b36162324b09c9330f326fa917e30c5cc2d2
dwstallings <dwstallings@email.arizona.edu>
2018-02-17 19:27:00 -0700

    Here are the cipher files

---
+++ Project1Stallings_Sears/Ciphers.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include "Frequency.h"
+#include "Ciphers.h"
+#include <iostream> 
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+using namespace std;
+
+void shiftCipher(MonoDigrams mostFrequent, string cipher) {
+	char letter = mostFrequent.getLetters; //will identify what the letter is that is most frequent
+	int key = letter - 69;//letter is converted to its respective ASCII value and is subtracted by the ASCII for E
+	if (key < 0) {//if the key happens to be negative, we add 25 to it and shift that many times
+		key += 25;
+	}
+	for (int i = 0; i < cipher.length(); i++) {//go through the entire cipher text string
+		char temp = cipher[i];//grab each individual character
+		temp -= key; //subtract the character by the key value
+		if (temp < 65) { //if the character shift is less than the ASCII value for A, then add 26 to bring us back into the alphabet
+			temp += 26;
+		}
+		cout << temp; //output the corresponding text
+	}
+}
+
+void substitutionCipher() {
+
+}
---
+++ Project1Stallings_Sears/Ciphers.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef CIPHERS_H_
+#define CIPHERS_H_
+
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include "Frequency.h"
+
+void shiftCipher(MonoDigrams mostFrequent, string cipher); //will attempt to decode cipher using shift cipher
+void substitutionCipher(); //will attempt to decode cipher using substitution cipher
+#endif
